ae70be4e779f4a31c71068f4239838f0edde12eb
emeksdot <emeksdot@yahoo.com>
2024-06-21 19:08:56 +0100

    ready for the final audit

--- scripts/place.js
+++ scripts/place.js
@@ -8,12 +8,18 @@ lastModfiedDate.innerHTML = new Date(document.lastModified);
 
 // If using ºC and km/h: Wind chill temperature = 13.12 + 0.6215T - 11.37V0.16 + 0.3965TV0.16
 const calculateWindChill = (temperature, windSpeed) => {
-  let windChill =
-    13.12 +
-    0.6215 * temperature -
-    11.37 * windSpeed ** 0.16 +
-    0.3965 * temperature * windSpeed ** 0.16;
-  return windChill;
+  let windChill = "";
+  if (temperature <= 10 && windSpeed > 4.8) {
+    windChill =
+      13.12 +
+      0.6215 * temperature -
+      11.37 * windSpeed ** 0.16 +
+      0.3965 * temperature * windSpeed ** 0.16;
+    return windChill.toFixed(2);
+  } else {
+    return "N/A";
+  }
 };
 
-calculateWindChill(10, 4.8);
+const windChillFactor = document.querySelector(".windChillFactor");
+windChillFactor.textContent = calculateWindChill(27, 5);
